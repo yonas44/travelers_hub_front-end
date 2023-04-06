f589b9e2451f16da57e75708874255454797f1ef
yonas44 <tesfu.yonas1350@gmail.com>
2023-04-06 09:53:46 +0300

    Create reservation component along with filter options

---
+++ src/components/filterOptions.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import {
+  filterReservations,
+  selectedReservations,
+} from '../feature/reservations/reservationSlice';
+
+const FilterOptions = () => {
+  const dispatch = useDispatch();
+
+  const handleSelect = (event) => {
+    dispatch(selectedReservations(event.target.value));
+  };
+
+  const handleSearch = (event) => {
+    dispatch(filterReservations(event.target.value));
+  };
+
+  return (
+    <div className="filter-options">
+      <select id="filter-by" onChange={handleSelect}>
+        <option>All</option>
+        <option>My</option>
+      </select>
+      <input
+        placeholder="Search reservations..."
+        id="search-bar"
+        onChange={handleSearch}
+      />
+    </div>
+  );
+};
+
+export default FilterOptions;
---
+++ src/components/reservations/reservations.js
@@ -0,0 +1,68 @@
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Outlet, useNavigate } from 'react-router-dom';
+import FilterOptions from '../filterOptions';
+import './reservations.css';
+import load from '../../assets/load.gif';
+import Reservation from './singleReservation';
+import getReservations from '../../feature/reservations/getReservations';
+
+const Reservations = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const [user, setUser] = useState();
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    if (!localStorage.getItem('token')) navigate('/login');
+    setUser(localStorage.getItem('user'));
+    dispatch(getReservations());
+  }, [dispatch]);
+
+  const allBookings = useSelector((state) => state.reservations);
+  const bookings = allBookings.data.filter((booking) => {
+    if (allBookings.selected === 'All') {
+      if (booking.package.title.toLowerCase().includes(allBookings.filterby)) {
+        return true;
+      }
+    } else if (
+      booking.user_id === Number(user)
+      && booking.package.title.toLowerCase().includes(allBookings.filterby)
+    ) {
+      return true;
+    }
+    return false;
+  });
+
+  return (
+    <main>
+      {allBookings.pending ? (
+        <div className="loading-wrapper">
+          <img id="loading-gif" src={load} alt="loading-img" />
+        </div>
+      ) : (
+        <>
+          <p className={`removed-message ${message && 'slide'}`}>{message}</p>
+          <FilterOptions />
+          <section className="reservations-holder">
+            {bookings.length ? (
+              bookings.map((booking) => (
+                <Reservation
+                  key={booking.id}
+                  booking={booking}
+                  setMessage={setMessage}
+                  user={user}
+                />
+              ))
+            ) : (
+              <p>{allBookings.message}</p>
+            )}
+          </section>
+        </>
+      )}
+      <Outlet />
+    </main>
+  );
+};
+
+export default Reservations;
