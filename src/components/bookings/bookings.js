128e24ac1b8dbef8eb329124ee14282209f0b804
Selma <selmandapanda@yahoo.com>
2023-04-06 12:07:12 +0200

    Add Booking form to handle dates

---
+++ src/components/bookings/bookings.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import DatePicker from 'react-multi-date-picker';
+import './bookings.css';
+
+const BookingForm = () => {
+  const [startDate, setStartDate] = useState('Choose Start Date');
+  const [endDate, setEndDate] = useState('Choose End Date');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className="container">
+      <div>
+        <p className="form-title form-title-1">Collect Moments,</p>
+        <p className="form-title form-title-2">Not Things</p>
+      </div>
+
+      <form className="booking-form" onSubmit={handleSubmit}>
+        <div className="start-date">
+          <DatePicker
+            inputClass="start-date-input"
+            value={startDate}
+            onChange={setStartDate}
+            format="DD/MM/YYYY"
+            placeholder="Start Date"
+          />
+        </div>
+
+        <div className="end-date">
+          <DatePicker
+            inputClass="end-date-input"
+            value={endDate}
+            onChange={setEndDate}
+            format="DD/MM/YYYY"
+            placeholder="End Date"
+          />
+        </div>
+      </form>
+    </div>
+  );
+};
