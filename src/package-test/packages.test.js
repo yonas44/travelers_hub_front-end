2290f4496d3b63953361f83fc7795d4f0fa25c14
ifeanyi305 <105528809+ifeanyi305@users.noreply.github.com>
2023-04-10 20:39:49 +0100

    add test to components

---
+++ src/package-test/packages.test.js
@@ -0,0 +1,17 @@
+import {render, screen} from '@testing-library/react';
+import { Provider } from 'react-redux';
+import store from '../redux/configureStore';
+import Package from '../pages/Package';
+
+describe('Packages', () => {
+  it('renders package component', () => {
+    render(
+      <Provider store={store}>
+        <Package />
+      </Provider>,
+    )
+    screen.getByRole('img', {
+      name: /loading-giphy/i
+    })
+  })
+})
