d33e764a4196fc8925f5bc7c973197d62ce299ba
yonas44 <tesfu.yonas1350@gmail.com>
2023-04-10 23:39:07 +0300

    Add test for filtering records feature in reservations component

--- src/__tests__/reservations_interactions.test.js
+++ src/__tests__/reservations_interactions.test.js
@@ -1,4 +1,9 @@
-import { render, screen } from '@testing-library/react';
+import {
+  findByPlaceholderText,
+  fireEvent,
+  render,
+  screen,
+} from '@testing-library/react';
 import { rest } from 'msw';
 import { setupServer } from 'msw/node';
 import { Provider } from 'react-redux';
@@ -8,29 +13,50 @@ import store from '../redux/configureStore';
 
 // Mock API calls
 const handlers = [
-  rest.get('http://127.0.0.1:3000/bookings', (req, res, ctx) => res(
-    ctx.json([
-      {
-        id: 272,
-        user_id: 2,
-        package: {
-          id: 2,
+  rest.get('http://127.0.0.1:3000/bookings', (req, res, ctx) =>
+    res(
+      ctx.json([
+        {
+          id: 272,
           user_id: 2,
-          title: 'Wanka Resort',
-          description: 'Random place with nice views',
-          destination: 'Madagascar',
-          photo: ['http://image1.png', 'http://image2.png'],
-          flight: true,
-          price: '3400.0',
-          bus: false,
-          accomodation: 't',
-          promotion: 0,
-          created_at: '2023-04-08T19:17:36.261Z',
-          updated_at: '2023-04-08T19:17:36.261Z',
+          package: {
+            id: 2,
+            user_id: 2,
+            title: 'Wanka Resort',
+            description: 'Random place with nice views',
+            destination: 'Madagascar',
+            photo: ['http://image1.png', 'http://image2.png'],
+            flight: true,
+            price: '3400.0',
+            bus: false,
+            accomodation: 't',
+            promotion: 0,
+            created_at: '2023-04-08T19:17:36.261Z',
+            updated_at: '2023-04-08T19:17:36.261Z',
+          },
         },
-      },
-    ]),
-  )),
+        {
+          id: 273,
+          user_id: 1,
+          package: {
+            id: 2,
+            user_id: 2,
+            title: 'Figi Resort',
+            description: 'Random place with nice views',
+            destination: 'Kenya',
+            photo: ['http://image1.png', 'http://image2.png'],
+            flight: true,
+            price: '3400.0',
+            bus: false,
+            accomodation: 't',
+            promotion: 0,
+            created_at: '2023-04-08T19:17:36.261Z',
+            updated_at: '2023-04-08T19:17:36.261Z',
+          },
+        },
+      ])
+    )
+  ),
 ];
 
 const server = setupServer(...handlers);
@@ -57,9 +83,25 @@ describe('Reservation component renders', () => {
         <BrowserRouter>
           <Reservations />
         </BrowserRouter>
-      </Provider>,
+      </Provider>
     );
 
     expect(await screen.findByText(/Madagascar/)).toBeInTheDocument();
+  });
+
+  it('filters based on the search', async () => {
+    render(
+      <Provider store={store}>
+        <BrowserRouter>
+          <Reservations />
+        </BrowserRouter>
+      </Provider>
+    );
+
+    fireEvent.change(await screen.findByTestId('search-bar'), {
+      target: { value: 'wanka' },
+    });
+
+    expect(screen.queryByText(/Kenya/)).not.toBeInTheDocument();
   });
 });
