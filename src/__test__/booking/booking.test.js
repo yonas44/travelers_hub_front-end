4a54c558e9797d093d047fb8481d3707d97deec2
Selma <selmandapanda@yahoo.com>
2023-04-12 23:47:37 +0200

    Fix linters errors

--- src/__test__/booking/booking.test.js
+++ src/__test__/booking/booking.test.js
@@ -1,25 +1,22 @@
-import { render, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
-import store from '../../redux/configureStore';
 import renderer from 'react-test-renderer';
+import store from '../../redux/configureStore';
 import BookingPage from '../../pages/BookingPage';
 import '@testing-library/jest-dom';
 
 describe('Booking component', () => {
   it('should match snapshot', () => {
     const tree = renderer
-    .create(
-      <Provider store={store}>
-        <BrowserRouter>
-          <BookingPage />
-        </BrowserRouter>
-      </Provider>,
-    )
-    .toJSON();
+      .create(
+        <Provider store={store}>
+          <BrowserRouter>
+            <BookingPage />
+          </BrowserRouter>
+        </Provider>,
+      )
+      .toJSON();
 
-  expect(tree).toMatchSnapshot();
-});
-
-  
+    expect(tree).toMatchSnapshot();
+  });
 });
